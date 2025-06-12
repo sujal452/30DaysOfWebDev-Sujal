792604b97c30efffec5c879552f3b4bc1903d962
Sujal <sujaldhiman71@gmail.com>
2025-06-12 10:30:14 +0530

    Day 1: JS Basics and Variables

---
+++ JavaScript_7Days/day1/script.js
@@ -0,0 +1,27 @@
+//script.js
+
+
+console.log("Day 1 JS-Basics");
+
+//variable
+
+let name ="sujal";
+var age =21;
+const city ="kangra";
+
+console.log(name,age,city);
+
+
+//data type 
+
+let num = 50;
+let text ="java script";
+let iscool = true;
+let x;
+let y = null;
+
+console.log(typeof num);
+console.log(typeof text);
+console.log(typeof iscool);
+console.log(typeof x);
+console.log(typeof y);
